60f6be16908e0d6eff693ff92d2eb140b234bea4
lostprobe99 <lostprobe@qq.com>
2021-12-18 21:42:34 +0800

    weak_ptr ver

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@
 
 struct Node {
     // 这两个指针会造成什么问题？请修复
+    // 产生了循环引用，a 指向 b，b 指向 a，引用计数无法清零
     std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
+    // std::shared_ptr<Node> prev;
+    std::weak_ptr<Node> prev;
+    // std::unique_ptr<Node> prev;
     // 如果能改成 unique_ptr 就更好了!
 
     int value;
@@ -17,21 +20,24 @@ struct Node {
         node->value = value;
         node->next = next;
         node->prev = prev;
-        if (prev)
-            prev->next = node;
+        if (!prev.expired()) // 检查如果该弱引用未失效
+            prev.lock()->next = node;   // 则生成一个临时的 shared_ptr 进行调用
+            // prev->next = node;
         if (next)
             next->prev = node;
     }
 
     void erase() {
-        if (prev)
-            prev->next = next;
+        if (!prev.expired())
+            prev.lock()->next = next;
+            // prev->next = next;
         if (next)
             next->prev = prev;
     }
 
     ~Node() {
         printf("~Node()\n");   // 应输出多少次？为什么少了？
+        // 产生了循环引用
     }
 };
 
@@ -42,11 +48,22 @@ struct List {
 
     List(List const &other) {
         printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
+        // head = other.head;  // 这是浅拷贝！
         // 请实现拷贝构造函数为 **深拷贝**
+        head = std::make_shared<Node>(other.head->value);
+        auto tail = head;
+        auto p = other.front()->next;
+        while(p != nullptr) {
+            auto node = std::make_shared<Node>(p->value);
+            tail->next = node;
+            node->prev = tail;
+            tail = node;
+            p = p->next;
+        }
     }
 
     List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？
+    // 当需要调用此函数时，编译器会构造一个临时对象，随后调用移动赋值函数
 
     List(List &&) = default;
     List &operator=(List &&) = default;
@@ -78,7 +95,8 @@ struct List {
     }
 };
 
-void print(List lst) {  // 有什么值得改进的？
+// 使用 const 引用避免传参时拷贝
+void print(const List& lst) {  // 有什么值得改进的？
     printf("[");
     for (auto curr = lst.front(); curr; curr = curr->next.get()) {
         printf(" %d", curr->value);
@@ -112,6 +130,7 @@ int main() {
 
     b = {};
     a = {};
+    // a 和 b 一共 11 次析构
 
     return 0;
 }
